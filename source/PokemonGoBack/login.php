bb0052b434af0a7e29583df669384e7e47925200
Ling Tan <lingtan.ca@gmail.com>
2018-06-02 20:23:41 -0400

    Update login.php

--- source/PokemonGoBack/login.php
+++ source/PokemonGoBack/login.php
@@ -38,16 +38,19 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
 	}else{
 		// invalid
 	}
+}else{
+	<form class="form-signin" action="LoginServlet" method="post">
+		<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+		<label for="username" class="sr-only">Username</label>
+		<input type="text" id="username" name="username" class="form-control" placeholder="Username: test" required autofocus>
+		<label for="inputPassword" class="sr-only">Password</label>
+		<input type="password" id="password" name="password" class="form-control" placeholder="Password: COMP354" required>
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+		<p class="mt-5 mb-3 text-muted">&copy; 2018 - 2019</p>
+	</form>
+}
 ?>
-    <form class="form-signin" action="LoginServlet" method="post">
-      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="username" class="sr-only">Username</label>
-      <input type="text" id="username" name="username" class="form-control" placeholder="Username: test" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="password" name="password" class="form-control" placeholder="Password: COMP354" required>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2018 - 2019</p>
-    </form>
+    
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
