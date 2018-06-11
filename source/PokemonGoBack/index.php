142b61933e4915776ffdce68cf5739a20b6c4eab
Ling Tan <lingtan.ca@gmail.com>
2018-06-11 00:13:25 -0400

    remvoe test

--- source/PokemonGoBack/index.php
+++ source/PokemonGoBack/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">aqx
+    <meta name="author" content="">
     <link rel="shortcut icon" href="image/favicon.ico" />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
