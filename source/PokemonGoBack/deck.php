7f4b7c1a9ae22ebd8f206e3fe88a11ee7edbca5c
Ling Tan <lingtan.ca@gmail.com>
2018-06-12 17:54:01 -0400

    Update deck.php

--- source/PokemonGoBack/deck.php
+++ source/PokemonGoBack/deck.php
@@ -19,8 +19,8 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 class="my-0 mr-md-auto font-weight-normal">Upload Deck</h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Link</a>
-            <a class="p-2 text-dark" href="#">Link</a>
+            <a class="p-2 text-dark" href="collection.php?action=update">Update Collection</a>
+	    <a class="p-2 text-dark" href="#">Update Deck</a>
 
             <?php
             require_once( 'util.php');
