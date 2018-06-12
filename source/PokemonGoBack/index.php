332d5a66d8ad8d717d589822b851a297db27125e
Ling Tan <lingtan.ca@gmail.com>
2018-06-12 17:42:06 -0400

    Update index.php

--- source/PokemonGoBack/index.php
+++ source/PokemonGoBack/index.php
@@ -19,8 +19,8 @@
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 class="my-0 mr-md-auto font-weight-normal">PokemonGoBack</h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="#">Link</a>
-            <a class="p-2 text-dark" href="#">Link</a>
+            <a class="p-2 text-dark" href="upload.php?action=update">Update Collection</a>
+            <a class="p-2 text-dark" href="update.php?action=update">Update Deck</a>
 
             <?php
             require_once( 'db.php');
