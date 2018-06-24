b4c88db700486b471361687ef5f343c05161b086
JiyeLi <kedazhou@hotmail.com>
2018-06-24 16:57:43 -0400

    Update go.js

--- source/PokemonGoBack/js/go.js
+++ source/PokemonGoBack/js/go.js
@@ -18,10 +18,27 @@ $(function () {
 	cardDeck.push(new Card('Energy', cardType.Energy, 0, 1, 0));
 	cardDeck.push(new Card('Energy', cardType.Energy, 0, 1, 0));
 
-	let player = new Player(cardDeck, cardDeck);
-	let ai = new Player(cardDeck, cardDeck);
-	let game = new Game(player, ai);
+	let cardDeckai = [];
+	cardDeckai.push(new Card('Pikachu', cardType.PokeMon, 60, 0, 3));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Zapdos', cardType.Energy, 120, 0, 4));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
+	cardDeckai.push(new Card('Energy', cardType.Energy, 0, 1, 0));
 
+	let player = new Player(cardDeck, cardDeck);
+	let ai = new Player(cardDeck, cardDeckai);
+	let game = new Game(player, ai);
+	
+	
+		
 	game.start();
 
 	//$( "#draggable" ).draggable({ axis: "y" });
@@ -29,50 +46,42 @@ $(function () {
  
     //$( "#draggable3" ).draggable({ containment: "#containment-wrapper", scroll: false });
 	//$( "#draggable4" ).draggable({ containment: "parent" });
+
+	//in order to enable/disable draggable during game , move all draggablefunction inside  game.js 
 	$( ".pokemonallcard" ).draggable({
-		appendTo: "body",
-		cursor: "move",
-		helper: 'clone',
-		revert: "invalid"
-	});
-	
-	$("#divCardActive").droppable(
-	{
-		tolerance: "intersect",
-		accept: ".pokemonallcard",
-		activeClass: "ui-state-default",
-		hoverClass: "ui-state-hover",
-		drop: function(event, ui) {        
-			$(this).append($(ui.draggable));
-		}
-	});
-/*	$("#svgCardMat").droppable(
-	{
-		tolerance: "intersect",
-		accept: ".pokemon",
-		activeClass: "ui-state-default",
-		hoverClass: "ui-state-hover",
-		
-		drop: function(event, ui) {        
-			$(this).append($(ui.draggable));
-			
-		}
-	});*/
-	$("#svgCardMat").droppable({
-		accept:".pokemonallcard",
-		tolerance: "intersect",
-		activeClass: "ui-state-default",
-		hoverClass: "ui-state-hover",
-		drop: function(event, ui) {
-			var $this = $(this);
-			ui.draggable.position({
-			my: "bottom",
-			at: "bottom",
-			of: $this,
-			using: function(pos) {
-				$(this).animate(pos, 100, "linear");
+			appendTo: "body",
+			cursor: "move",
+			helper: 'clone',
+			revert: "invalid",
+			//disabled: false
+		});
+	$("#divCardActive").droppable({
+			tolerance: "intersect",
+			accept: ".pokemonallcard",
+			activeClass: "ui-state-default",
+			hoverClass: "ui-state-hover",
+			drop: function(event, ui) {        
+				$(this).append($(ui.draggable));
+			}
+		});
+
+$("#svgCardMat").droppable({
+			accept:".pokemonallcard",
+			tolerance: "intersect",
+			activeClass: "ui-state-default",
+			hoverClass: "ui-state-hover",
+			drop: function(event, ui) {
+				var $this = $(this);
+				ui.draggable.position({
+					my: "bottom",
+					at: "bottom",
+					of: $this,
+					using: function(pos) {
+						$(this).animate(pos, 100, "linear");
+					}
+				})
 				}
-			});
-		}
-	});
+		});
+	
+	
 });
