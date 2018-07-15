7b10c795f96d4014dc47226f901ec8e72c0d9f94
Ling Tan <lingtan.ca@gmail.com>
2018-07-15 16:13:35 -0400

    Add files via upload

--- source/PokemonGoBack/header.php
+++ source/PokemonGoBack/header.php
@@ -25,6 +25,7 @@
 
 <script src="js/util.js"></script>
 <script src="js/Ability.js"></script>
+<script src="js/GameConsole.js"></script>
 <script src="js/Card.js"></script>
 <script src="js/Player.js"></script>
 <script src="js/Game.js"></script>
