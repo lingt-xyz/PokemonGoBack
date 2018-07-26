a2a44e3db587fa54e91c976f641c9477bc892539
Ling Tan <ta_li@encs.concordia.ca>
2018-07-26 19:31:39 -0400

    test

--- source/PokemonGoBack/js/Metadata.js
+++ source/PokemonGoBack/js/Metadata.js
@@ -136,7 +136,22 @@ Red Card:deck:target:opponent:destination:deck:count(opponent-hand),shuffle:targ
 Wally:search:target:choice:your-pokemon:cat:basic:source:deck:filter:evolves-from:target:last:1,shuffle:target:your
 `;
 
-var Card_Collection = new Array();
+let Card_Collection = new Array();
+
+let Card_Type = { pokemon: "pokemon", trainer: "trainer", energy: "energy" };
+let Energy_Type = { colorless: "colorless", water: "water", lightning: "lightning", psychic: "psychic", fighting: "fighting" };
+let Trainer_Type = { stadium: "stadium", supporter: "supporter", item: "item" };
+let Ability_Type = { dam: "dam", deenergize: "deenergize", redamage: "redamage", heal: "heal", deck: "deck", cond: "cond", search: "search", draw: "draw", applystat: "applystat", swap: "swap", reenergize: "reenergize", add: "add", shuffle: "shuffle", destat: "destat" };
+let Target_Pokemon = { opponent: "opponent", your_active: "your-active", opponent_active: "opponent-active", choice_opponent: "choice:opponent", choice_your: "choice:your", choice_opponent_bench: "choice:opponent-bench", choice_your_banch: "choice:your-bench" };
+let Target_Player = { you: "you", them: "them" };
+
+let user = null;
+let ai = null;
+let game = null;
+let currentPlayer = null;
+let logger = null;
+let userOrder = null;
+let aiOrder = null;
 
 function initCardCollection() {
     /**
@@ -421,18 +436,3 @@ function getSubAbility(s) {
             break;
     }
 }
-
-let Card_Type = { pokemon: "pokemon", trainer: "trainer", energy: "energy" };
-let Energy_Type = { colorless: "colorless", water: "water", lightning: "lightning", psychic: "psychic", fighting: "fighting" };
-let Trainer_Type = { stadium: "stadium", supporter: "supporter", item: "item" };
-let Ability_Type = { dam: "dam", deenergize: "deenergize", redamage: "redamage", heal: "heal", deck: "deck", cond: "cond", search: "search", draw: "draw", applystat: "applystat", swap: "swap", reenergize: "reenergize", add: "add", shuffle: "shuffle", destat: "destat" };
-let Target_Pokemon = { opponent: "opponent", your_active: "your-active", opponet_active: "opponent-active", choice_opponet: "choice:opponet", choice_your: "choice:your", choice_opponet_bench: "choice:opponet-bench", choice_your_banch: "choice:your-bench" };
-let Target_Player = { you: "you", them: "them" };
-
-let user = null;
-let ai = null;
-let game = null;
-let currentPlayer = null;
-let logger = null;
-let userOrder = null;
-let aiOrder = null;
--- source/PokemonGoBack/js/go.js
+++ source/PokemonGoBack/js/go.js
@@ -73,7 +73,164 @@ function applyAbility(id, isAi, abilityIndex) {
 function useAbility(sourceCard, abilityIndex){
 	let ability = Ability_Collection[abilityIndex];
 	for (let sub of ability.subAbilities) {
-		attack(sourceCard, sub);
+		gatherBattleInfo(sourceCard, sub);
+	}
+}
+
+function gatherBattleInfo(sourceCard, sub){
+	let you = null;
+	let opponent = null;
+	if (sourceCard.isAi) {
+		you = ai;
+		opponent = user;
+	} else {
+		you = user;
+		opponent = ai;
+	}
+
+	if (sub instanceof Dam) {
+		logger.logBattle("!!!Dam!!!");
+		let damHp = 0;
+		if (sub.damHp.contains("count")) {
+			let ss = sub.damHp.split("*");
+			let num1 = 0;
+			let num2 = 0;
+			let string2 = "";
+			if (ss[0].contains("count")) {
+				num1 = ss[1];
+				string2 = ss[0];
+			} else {
+				num1 = ss[0];
+				string2 = ss[1];
+			}
+			// TODO remove strings
+			switch (string2) {
+				case "your-bench":
+					num2 = you.benchCollection.length;
+					break;
+				case "your-active:damage":
+					if (you.currentPokemon) {
+						num2 = you.currentPokemon.damage;
+					} else {
+						num2 = 0;
+					}
+					break;
+				case "opponent-active:energy":
+					if (you.currentPokemon) {
+						num2 = you.currentPokemon.energy + you.currentPokemon.currentColorLessEnergy;
+					} else {
+						num2 = 0;
+					}
+					break;
+				default:
+					break;
+			}
+			damHp = num1 * num2;
+		} else {
+			damHp = sub.damHp;
+		}
+		switch (sub.target) {
+			case Target_Pokemon.opponent:
+				dam(opponent.handCollection);
+				dam(opponent.matCollection);
+				dam(opponent.benchCollection);
+				break;
+			case Target_Pokemon.opponent_active:
+				your.currentPokemon.currentHp -= damHp;
+				break;
+			case Target_Pokemon.your_active:
+				me.currentPokemon.currentHp -= damHp;
+				break;
+			case Target_Pokemon.choice_opponent:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_your:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_opponent_bench:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_your_banch:
+				// TODO choose one card
+				break;
+			default:
+				break;
+		}
+	} else if (sub instanceof Heal) {
+		target.hp += sub.number;
+		switch (sub.target) {
+			case Target_Pokemon.opponent_active:
+				//
+				break;
+			case Target_Pokemon.your_active:
+				me.currentPokemon.currentHp += sub.number;
+				break;
+			case Target_Pokemon.choice_opponent:
+				// 
+				break;
+			case Target_Pokemon.choice_your:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_opponent_bench:
+				// 
+				break;
+			case Target_Pokemon.choice_your_banch:
+				// 
+				break;
+			default:
+				break;
+		}
+	} else if (sub instanceof Deenergize) {
+		switch (sub.target) {
+			case Target_Pokemon.opponent_active:
+				
+				break;
+			case Target_Pokemon.your_active:
+				
+				break;
+			case Target_Pokemon.choice_opponent:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_your:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_opponent_bench:
+				// TODO choose one card
+				break;
+			case Target_Pokemon.choice_your_banch:
+				// TODO choose one card
+				break;
+			default:
+				break;
+		}
+	} else if (sub instanceof Reenergize) {
+		
+	} else if (sub instanceof Swap) {
+		
+	} else if (sub instanceof Destat) {
+		
+	} else if (sub instanceof ApplyStat) {
+		
+	} else if (sub instanceof Draw) {
+		
+	} else if (sub instanceof Redamage) {
+		
+	} else if (sub instanceof Search) {
+		
+	} else if (sub instanceof Deck) {
+		
+	} else if (sub instanceof Shuffle) {
+		
+	} else if (sub instanceof Add) {
+		
+	}
+}
+
+function dam(target, amount){
+	if(target instanceof Pokemon){
+
+	}else{
+
 	}
 }
 
@@ -94,7 +251,6 @@ function attack(sourceCard, sub) {
 	}
 
 	if (sub instanceof Dam) {
-		logger.logBattle("!!!Dam!!!");
 		let damHp = 0;
 		if (sub.damHp.contains("count")) {
 			let ss = sub.damHp.split("*");
@@ -138,19 +294,19 @@ function attack(sourceCard, sub) {
 			case Target_Pokemon.opponent:
 				//TODO
 				break;
-			case Target_Pokemon.opponet_active:
+			case Target_Pokemon.opponent_active:
 				your.currentPokemon.currentHp -= damHp;
 				break;
 			case Target_Pokemon.your_active:
 				me.currentPokemon.currentHp -= damHp;
 				break;
-			case Target_Pokemon.choice_opponet:
+			case Target_Pokemon.choice_opponent:
 				// TODO choose one card
 				break;
 			case Target_Pokemon.choice_your:
 				// TODO choose one card
 				break;
-			case Target_Pokemon.choice_opponet_bench:
+			case Target_Pokemon.choice_opponent_bench:
 				// TODO choose one card
 				break;
 			case Target_Pokemon.choice_your_banch:
@@ -162,19 +318,19 @@ function attack(sourceCard, sub) {
 	} else if (sub instanceof Heal) {
 		target.hp += sub.number;
 		switch (sub.target) {
-			case Target_Pokemon.opponet_active:
+			case Target_Pokemon.opponent_active:
 				//
 				break;
 			case Target_Pokemon.your_active:
 				me.currentPokemon.currentHp += sub.number;
 				break;
-			case Target_Pokemon.choice_opponet:
+			case Target_Pokemon.choice_opponent:
 				// 
 				break;
 			case Target_Pokemon.choice_your:
 				// TODO choose one card
 				break;
-			case Target_Pokemon.choice_opponet_bench:
+			case Target_Pokemon.choice_opponent_bench:
 				// 
 				break;
 			case Target_Pokemon.choice_your_banch:
@@ -185,19 +341,19 @@ function attack(sourceCard, sub) {
 		}
 	} else if (sub instanceof Deenergize) {
 		switch (sub.target) {
-			case Target_Pokemon.opponet_active:
+			case Target_Pokemon.opponent_active:
 				
 				break;
 			case Target_Pokemon.your_active:
 				
 				break;
-			case Target_Pokemon.choice_opponet:
+			case Target_Pokemon.choice_opponent:
 				// TODO choose one card
 				break;
 			case Target_Pokemon.choice_your:
 				// TODO choose one card
 				break;
-			case Target_Pokemon.choice_opponet_bench:
+			case Target_Pokemon.choice_opponent_bench:
 				// TODO choose one card
 				break;
 			case Target_Pokemon.choice_your_banch:
