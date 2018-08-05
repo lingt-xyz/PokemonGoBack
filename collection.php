c69a4772a044b33aaf84a1960c5226a68da699ce
Ling Tan <ta_li@encs.concordia.ca>
2018-08-05 17:05:00 -0400

    test

--- collection.php
+++ collection.php
@@ -26,6 +26,7 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
     <h5 class="my-0 mr-md-auto font-weight-normal">Upload Card Collection</h5>
     <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="\">Home</a>
         <a class="p-2 text-dark" href="#">Update Collection</a>
         <a class="p-2 text-dark" href="deck.php?action=update">Update Deck</a>
 
